401f984172139a0d1937ee0fbc78c25ef7968dba
lilibella <lilibella28@hotmail.com>
2023-04-23 15:33:02 -0400

    rearrange components

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,61 +1,57 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import ProjectCard from './ProjectCards';
+import Particle from '../Particle';
+import editor from '../../Assets/Projects/jobtracker.png';
+import picome from '../../Assets/Projects/picome.png';
+import mancala from '../../Assets/Projects/mancala.png';
 
 function Projects() {
-  return (
-    <Container fluid className="project-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-         <strong className="purple">Projects</strong>
-        </h1>
-        
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="PicoMe"
-              description="PicoMe is a browser-based application that utilizes mongoose to allow users to perform CRUD operations on a MongoDB database. PicoMe mission is to create a platform that allows users to share or request food assistance from the community without cost."
-              ghLink="https://github.com/lilibella28/PicoMe"
-              demoLink="https://picome-production.up.railway.app/orders"
-            />
-          </Col>
+	return (
+		<Container fluid className="project-section">
+			<Particle />
+			<Container>
+				<h1 className="project-heading">
+					<strong className="purple">Projects</strong>
+				</h1>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Mancala"
-              description="The mancala games are a family of two-player turn-based strategy board games played with small stones, beans, or seeds and rows of holes or pits in the earth, a board or other playing surface. The objective is usually to capture all or some set of the opponent's pieces."
-              ghLink="https://github.com/lilibella28/MancalaGame"
-              demoLink="https://lilibella28.github.io/MancalaGame/"
-            />
-          </Col>
+				<Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+					<Col md={4} className="project-card">
+						<ProjectCard
+							imgPath={picome}
+							isBlog={false}
+							title="PicoMe"
+							description="PicoMe is a browser-based application that utilizes mongoose to allow users to perform CRUD operations on a MongoDB database. PicoMe mission is to create a platform that allows users to share or request food assistance from the community without cost."
+							ghLink="https://github.com/lilibella28/PicoMe"
+							demoLink="https://picome-production.up.railway.app/orders"
+						/>
+					</Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Job Tracker"
-              description="This mobile responsive application is meant as a Job Application tracking tool. Helping new grads, career transitioners, and veterans in their prospective fields keep track of their submitted applications."
-              ghLink="https://github.com/lilibella28/Job-Tracker"
-              demoLink="https://job-tracker-production.up.railway.app/"             
-            />
-          </Col>
+					<Col md={4} className="project-card">
+						<ProjectCard
+							imgPath={mancala}
+							isBlog={false}
+							title="Mancala"
+							description="The mancala games are a family of two-player turn-based strategy board games played with small stones, beans, or seeds and rows of holes or pits in the earth, a board or other playing surface. The objective is usually to capture all or some set of the opponent's pieces."
+							ghLink="https://github.com/lilibella28/MancalaGame"
+							demoLink="https://lilibella28.github.io/MancalaGame/"
+						/>
+					</Col>
 
-     
-
-        </Row>
-      </Container>
-     
-    </Container>
-  );
+					<Col md={4} className="project-card">
+						<ProjectCard
+							imgPath={editor}
+							isBlog={false}
+							title="Job Tracker"
+							description="This mobile responsive application is meant as a Job Application tracking tool. Helping new grads, career transitioners, and veterans in their prospective fields keep track of their submitted applications."
+							ghLink="https://github.com/lilibella28/Job-Tracker"
+							demoLink="https://job-tracker-production.up.railway.app/"
+						/>
+					</Col>
+				</Row>
+			</Container>
+		</Container>
+	);
 }
 
 export default Projects;
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Particle from '../Particle';
 import pdf from '../../Assets/../Assets/LilibehtAMontanoResumePortFolio.pdf';
@@ -22,18 +22,22 @@ function ResumeNew() {
 		<div>
 			<Container fluid className="resume-section">
 				<Particle />
-				<Row style={{ justifyContent: 'center', position: 'relative' }}>
-					<Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: '250px' }}>
-						<AiOutlineDownload />
-						&nbsp;Download Resume
-					</Button>
-				</Row>
+				
 
 				<Row className="resume">
+					<Col>
 					<Document file={resumeLink} className="d-flex justify-content-center">
 						<Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
 					</Document>
+					</Col>
+
+					<Col><Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: '250px', marginTop: '10px' }}>
+						<AiOutlineDownload />
+						&nbsp;Download Resume
+					</Button> </Col>
+					
 				</Row>
+				
 
 				<Row style={{ justifyContent: 'center', position: 'relative' }}>
 					<Calendly />
